44fe7c1f2f68fe0d69677d4db7a81240c7989983
stognienkovladislav <stognienkovv@gmail.com>
2017-08-19 21:24:22 +0300

    Comm

--- Excel/Script2.py
+++ Excel/Script2.py
@@ -1,6 +1,5 @@
-
-
-from openpyxl import load_workbook
+from itertools import islice
+from openpyxl import load_workbook, Workbook
 
 #Load in the workbook
 
@@ -64,4 +63,64 @@ print(get_column_letter(1))
 
 #Return '1'
 print(column_index_from_string('A'))
+
+
+
+#Print row per row
+for cellObj in sheet['A1':'C3']:
+    for cell in cellObj:
+        print(cell.coordinate, cell.value)
+
+    print('----END----')
+
+
+
+#Retrieve the maximum amount of rows
+print(sheet.max_row)
+
+#Retrieve the maximum amount of column
+print(sheet.max_column)
+
+
+
+############################################
+
+import pandas as pd
+
+#Convert Sheet to DataFrame
+df = pd.DataFrame(sheet.values)
+
+#Put the sheet values in `data`
+data = sheet.values
+
+#Indicate the columns in the sheet values
+cols = next(data)[1:]
+
+#Convert your data to a list
+data = list(data)
+
+idx = [r[0] for r in data]
+
+#Slice the data at index 1
+
+data = (islice(r, 1, None)for r in data)
+
+#Make your DataFrame
+df = pd.DataFrame(data, index = idx, columns = cols)
+
+
+
+#Import `dataframe_to_rows`
+from openpyxl.utils.dataframe import  dataframe_to_rows
+
+#Initialize a workbook
+wb = Workbook()
+
+#Get the workshet in the active workbook
+ws = wb.active
+
+
+#Append the rows of the DataFrame to your worksheet
+for r in dataframe_to_rows(df, index = True, header = True):
+    ws.append(r)
 
